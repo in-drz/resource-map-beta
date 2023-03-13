dfd28f1dbd0c766312580951e04fecebdc584d67
in-drz <90423194+in-drz@users.noreply.github.com>
2023-03-13 16:20:33 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ const config = {
   style: 'mapbox://styles/mapbox/light-v11',
   accessToken:
     'pk.eyJ1IjoiaW4tZHJ6IiwiYSI6ImNsZXhxdWNmOTFxZDQzdnFvYmRhdHl0czQifQ.fJZWF9JhBmdx-DbEY4deow',
-  CSV: 'https://docs.google.com/spreadsheets/d/1YvbSE3Lg-osHDwpM3_wCIdLEBk2riEFbu7j2Il1xogc/gviz/tq?tqx=out:csv&sheet=resource_responders',
   center: [-118.254643, 34.049157],
   zoom: 10,
   title: 'Resource Responders',
