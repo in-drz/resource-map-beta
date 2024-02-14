e8d5b1ba7ea813ea4a294bcd9b04efed56a0ce5c
Drazen Medina <drazenmedina@Drazens-MacBook-Pro.local>
2024-02-13 18:48:08 -0800

    Your commit message here

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-/* global config csv2geojson turf Assembly $ */
+/* global config csv2geojson turf Assembly $ mapboxgl */
+
 'use strict';
 
 mapboxgl.accessToken = config.accessToken;
 const columnHeaders = config.sideBarInfo;
-
-let geojsonData = {};
-const filteredGeojson = {
-  type: 'FeatureCollection',
-  features: [],
-};
+let geojsonData = {}; // Define geojsonData as a global variable
 
 const map = new mapboxgl.Map({
   container: 'map',
@@ -23,9 +19,7 @@ map.addControl(
     positionOptions: {
       enableHighAccuracy: true
     },
-    // When active the map will receive updates to the device's location as it changes.
     trackUserLocation: true,
-    // Draw an arrow next to the location dot to indicate which direction the device is heading.
     showUserHeading: true
   })
 );
@@ -39,7 +33,6 @@ function flyToLocation(currentFeature) {
 
 function createPopup(currentFeature) {
   const popups = document.getElementsByClassName('mapboxgl-popup');
-  /** Check if there is already a popup on the map and if so, remove it */
   if (popups[0]) popups[0].remove();
   new mapboxgl.Popup({ closeOnClick: true })
     .setLngLat(currentFeature.geometry.coordinates)
@@ -52,34 +45,27 @@ function createPopup(currentFeature) {
 }
 
 function buildLocationList(locationData) {
-  /* Add a new listing section to the sidebar. */
   const listings = document.getElementById('listings');
   listings.innerHTML = '';
   locationData.features.forEach((location, i) => {
     const prop = location.properties;
 
     const listing = listings.appendChild(document.createElement('div'));
-    /* Assign a unique `id` to the listing. */
     listing.id = 'listing-' + prop.id;
-
-    /* Assign the `item` class to each listing for styling. */
     listing.className = 'item';
 
-    /* Add the link to the individual listing created above. */
     const link = listing.appendChild(document.createElement('button'));
     link.className = 'title';
     link.id = 'link-' + prop.id;
     link.innerHTML =
       '<p style="line-height: 1.25">' + prop[columnHeaders[0]] + '</p>';
 
-    /* Add details to the individual listing. */
     const details = listing.appendChild(document.createElement('div'));
     details.className = 'content';
 
     for (let i = 1; i < columnHeaders.length; i++) {
       const div = document.createElement('div');
       div.innerText += prop[columnHeaders[i]];
-      div.className;
       details.appendChild(div);
     }
 
@@ -96,7 +82,7 @@ function buildLocationList(locationData) {
 
       const divList = document.querySelectorAll('.content');
       const divCount = divList.length;
-      for (i = 0; i < divCount; i++) {
+      for (let i = 0; i < divCount; i++) {
         divList[i].style.maxHeight = null;
       }
 
@@ -114,277 +100,10 @@ function buildLocationList(locationData) {
   });
 }
 
-// Build dropdown list function
-// title - the name or 'category' of the selection e.g. 'Languages: '
-// defaultValue - the default option for the dropdown list
-// listItems - the array of filter items
-
-function buildDropDownList(title, listItems) {
-  const filtersDiv = document.getElementById('filters');
-  const mainDiv = document.createElement('div');
-  const filterTitle = document.createElement('h3');
-  filterTitle.innerText = title;
-  filterTitle.classList.add('py12', 'txt-bold');
-  mainDiv.appendChild(filterTitle);
-
-  const selectContainer = document.createElement('div');
-  selectContainer.classList.add('select-container', 'center');
-
-  const dropDown = document.createElement('select');
-  dropDown.classList.add('select', 'filter-option');
-
-  const selectArrow = document.createElement('div');
-  selectArrow.classList.add('select-arrow');
-
-  const firstOption = document.createElement('option');
-
-  dropDown.appendChild(firstOption);
-  selectContainer.appendChild(dropDown);
-  selectContainer.appendChild(selectArrow);
-  mainDiv.appendChild(selectContainer);
-
-  for (let i = 0; i < listItems.length; i++) {
-    const opt = listItems[i];
-    const el1 = document.createElement('option');
-    el1.textContent = opt;
-    el1.value = opt;
-    dropDown.appendChild(el1);
-  }
-  filtersDiv.appendChild(mainDiv);
-}
-
-// Build checkbox function
-// title - the name or 'category' of the selection e.g. 'Languages: '
-// listItems - the array of filter items
-// To DO: Clean up code - for every third checkbox, create a div and append new checkboxes to it
-
-function buildCheckbox(title, listItems) {
-  const filtersDiv = document.getElementById('filters');
-  const mainDiv = document.createElement('div');
-  const filterTitle = document.createElement('div');
-  const formatcontainer = document.createElement('div');
-  filterTitle.classList.add('center', 'flex-parent', 'py12', 'txt-bold');
-  formatcontainer.classList.add(
-    'center',
-    'flex-parent',
-    'flex-parent--column',
-    'px3',
-    'flex-parent--space-between-main',
-  );
-  const secondLine = document.createElement('div');
-  secondLine.classList.add(
-    'center',
-    'flex-parent',
-    'py12',
-    'px3',
-    'flex-parent--space-between-main',
-  );
-  filterTitle.innerText = title;
-  mainDiv.appendChild(filterTitle);
-  mainDiv.appendChild(formatcontainer);
-
-  for (let i = 0; i < listItems.length; i++) {
-    const container = document.createElement('label');
-
-    container.classList.add('checkbox-container');
-
-    const input = document.createElement('input');
-    input.classList.add('px12', 'filter-option');
-    input.setAttribute('type', 'checkbox');
-    input.setAttribute('id', listItems[i]);
-    input.setAttribute('value', listItems[i]);
-
-    const checkboxDiv = document.createElement('div');
-    const inputValue = document.createElement('p');
-    inputValue.innerText = listItems[i];
-    checkboxDiv.classList.add('checkbox', 'mr6');
-    checkboxDiv.appendChild(Assembly.createIcon('check'));
-
-    container.appendChild(input);
-    container.appendChild(checkboxDiv);
-    container.appendChild(inputValue);
-
-    formatcontainer.appendChild(container);
-  }
-  filtersDiv.appendChild(mainDiv);
-}
-
-const selectFilters = [];
-const checkboxFilters = [];
-
-function createFilterObject(filterSettings) {
-  filterSettings.forEach((filter) => {
-    if (filter.type === 'checkbox') {
-      const keyValues = {};
-      Object.assign(keyValues, {
-        header: filter.columnHeader,
-        value: filter.listItems,
-      });
-      checkboxFilters.push(keyValues);
-    }
-    if (filter.type === 'dropdown') {
-      const keyValues = {};
-      Object.assign(keyValues, {
-        header: filter.columnHeader,
-        value: filter.listItems,
-      });
-      selectFilters.push(keyValues);
-    }
-  });
-}
-
-function applyFilters() {
-  const filterForm = document.getElementById('filters');
-
-  filterForm.addEventListener('change', function () {
-    const filterOptionHTML = this.getElementsByClassName('filter-option');
-    const filterOption = [].slice.call(filterOptionHTML);
-
-    const geojSelectFilters = [];
-    const geojCheckboxFilters = [];
-
-    filteredGeojson.features = [];
-    // const filteredFeatures = [];
-    // filteredGeojson.features = [];
-
-    filterOption.forEach((filter) => {
-      if (filter.type === 'checkbox' && filter.checked) {
-        checkboxFilters.forEach((objs) => {
-          Object.entries(objs).forEach(([, value]) => {
-            if (value.includes(filter.value)) {
-              const geojFilter = [objs.header, filter.value];
-              geojCheckboxFilters.push(geojFilter);
-            }
-          });
-        });
-      }
-      if (filter.type === 'select-one' && filter.value) {
-        selectFilters.forEach((objs) => {
-          Object.entries(objs).forEach(([, value]) => {
-            if (value.includes(filter.value)) {
-              const geojFilter = [objs.header, filter.value];
-              geojSelectFilters.push(geojFilter);
-            }
-          });
-        });
-      }
-    });
-
-    if (geojCheckboxFilters.length === 0 && geojSelectFilters.length === 0) {
-      geojsonData.features.forEach((feature) => {
-        filteredGeojson.features.push(feature);
-      });
-    } else if (geojCheckboxFilters.length > 0) {
-      geojCheckboxFilters.forEach((filter) => {
-        geojsonData.features.forEach((feature) => {
-          if (feature.properties[filter[0]].includes(filter[1])) {
-            if (
-              filteredGeojson.features.filter(
-                (f) => f.properties.id === feature.properties.id,
-              ).length === 0
-            ) {
-              filteredGeojson.features.push(feature);
-            }
-          }
-        });
-      });
-      if (geojSelectFilters.length > 0) {
-        const removeIds = [];
-        filteredGeojson.features.forEach((feature) => {
-          let selected = true;
-          geojSelectFilters.forEach((filter) => {
-            if (
-              feature.properties[filter[0]].indexOf(filter[1]) < 0 &&
-              selected === true
-            ) {
-              selected = false;
-              removeIds.push(feature.properties.id);
-            } else if (selected === false) {
-              removeIds.push(feature.properties.id);
-            }
-          });
-        });
-        let uniqueRemoveIds = [...new Set(removeIds)];
-        uniqueRemoveIds.forEach(function (id) {
-          const idx = filteredGeojson.features.findIndex(
-            (f) => f.properties.id === id,
-          );
-          filteredGeojson.features.splice(idx, 1);
-        });
-      }
-    } else {
-      geojsonData.features.forEach((feature) => {
-        let selected = true;
-        geojSelectFilters.forEach((filter) => {
-          if (
-            !feature.properties[filter[0]].includes(filter[1]) &&
-            selected === true
-          ) {
-            selected = false;
-          }
-        });
-        if (
-          selected === true &&
-          filteredGeojson.features.filter(
-            (f) => f.properties.id === feature.properties.id,
-          ).length === 0
-        ) {
-          filteredGeojson.features.push(feature);
-        }
-      });
-    }
-
-    map.getSource('locationData').setData(filteredGeojson);
-    buildLocationList(filteredGeojson);
-  });
-}
-
-function filters(filterSettings) {
-  filterSettings.forEach((filter) => {
-    if (filter.type === 'checkbox') {
-      buildCheckbox(filter.title, filter.listItems);
-    } else if (filter.type === 'dropdown') {
-      buildDropDownList(filter.title, filter.listItems);
-    }
-  });
-}
-
-function removeFilters() {
-  const input = document.getElementsByTagName('input');
-  const select = document.getElementsByTagName('select');
-  const selectOption = [].slice.call(select);
-  const checkboxOption = [].slice.call(input);
-  filteredGeojson.features = [];
-  checkboxOption.forEach((checkbox) => {
-    if (checkbox.type === 'checkbox' && checkbox.checked === true) {
-      checkbox.checked = false;
-    }
-  });
-
-  selectOption.forEach((option) => {
-    option.selectedIndex = 0;
-  });
-
-  map.getSource('locationData').setData(geojsonData);
-  buildLocationList(geojsonData);
-}
-
-function removeFiltersButton() {
-  const removeFilter = document.getElementById('removeFilters');
-  removeFilter.addEventListener('click', () => {
-    removeFilters();
-  });
-}
-
-createFilterObject(config.filters);
-applyFilters();
-filters(config.filters);
-removeFiltersButton();
-
 const geocoder = new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken, // Set the access token
-  mapboxgl: mapboxgl, // Set the mapbox-gl instance
-  marker: true, // Use the geocoder's default marker style
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl,
+  marker: true,
   zoom: 11,
 });
 
@@ -412,7 +131,7 @@ function sortByDistance(selectedPoint) {
     if (a.properties.distance < b.properties.distance) {
       return -1;
     }
-    return 0; // a must be equal to b
+    return 0;
   });
   const listings = document.getElementById('listings');
   while (listings.firstChild) {
@@ -424,139 +143,56 @@ function sortByDistance(selectedPoint) {
 geocoder.on('result', (ev) => {
   const searchResult = ev.result.geometry;
   sortByDistance(searchResult);
-
- /* const options = { units: 'miles' };
-  for (const store of stores.features) {
-    store.properties.distance = turf.distance(
-      searchResult,
-      store.geometry,
-      options
-  );
-} */
 });
 
 map.on('load', () => {
   map.addControl(geocoder, 'top-right');
-
-  // csv2geojson - following the Sheet Mapper tutorial https://www.mapbox.com/impact-tools/sheet-mapper
-  'use strict';
-
   console.log('loaded');
+  makeGeoJSON(config.CSV1); // Load CSV1 when the map is loaded
+});
 
-  $(document).ready(() => {
-
-      console.log('ready');
-      let currentCSV = config.CSV1; // Set default CSV
-
-      $.ajax({
-          type: 'GET',
-          url: currentCSV,
-          dataType: 'text',
-          success: function (csvData) {
-              makeGeoJSON(csvData);
-          },
-          error: function (request, status, error) {
-              console.log(request);
-              console.log(status);
-              console.log(error);
-          },
-      });
-  });
-
-
-  function makeGeoJSON(currentCSV) {
-      $.ajax({
-          type: 'GET',
-          url: currentCSV,
-          dataType: 'text',
-          success: function (csvData) {
-              csv2geojson.csv2geojson(
-                  csvData,
-                  {
-                      latfield: 'Latitude',
-                      lonfield: 'Longitude',
-                      delimiter: ',',
-                  },
-                  (err, data) => {
-                      data.features.forEach((data, i) => {
-                          data.properties.id = i;
-                      });
-
-                      const geojsonData = data;
-
-                      // Add the layer to the map
-                      map.addLayer({
-                          id: 'locationData',
-                          type: 'circle',
-                          source: {
-                              type: 'geojson',
-                              data: geojsonData,
-                          },
-                          paint: {
-                              'circle-radius': 5, // size of circles
-                              'circle-color': '#3D2E5D', // color of circles
-                              'circle-stroke-color': 'white',
-                              'circle-stroke-width': 1,
-                              'circle-opacity': 0.7,
-                          },
-                      });
-                  },
-              );
-
-              // Add event listeners
-              // ...
-
-              buildLocationList(geojsonData);
-          },
-          error: function (request, status, error) {
-              console.log(request);
-              console.log(status);
-              console.log(error);
-          },
-      });
-  }
-
+$(document).ready(() => {
   $('#toggleButton1').click(() => {
-      if (map.getSource('locationData')) {
-          map.removeLayer('locationData');
-          map.removeSource('locationData');
-      }
-      makeGeoJSON(config.CSV1);
+    if (map.getSource('locationData')) {
+      map.removeLayer('locationData');
+      map.removeSource('locationData');
+    }
+    makeGeoJSON(config.CSV1);
   });
 
   $('#toggleButton2').click(() => {
-      if (map.getSource('locationData')) {
-          map.removeLayer('locationData');
-          map.removeSource('locationData');
-      }
-      makeGeoJSON(config.CSV2);
+    if (map.getSource('locationData')) {
+      map.removeLayer('locationData');
+      map.removeSource('locationData');
+    }
+    makeGeoJSON(config.CSV2);
   });
-
-  // Add more toggleButton handlers for additional CSVs if needed
 
   // Modal functionality
   // ...
 
   // CSV Dropdown change event
   document.getElementById('csvDropdown').addEventListener('change', function() {
-      const selectedCsv = this.value;
-      loadCsv(selectedCsv);
+    const selectedCsv = this.value;
+    loadCsv(selectedCsv);
   });
 
   // Load CSV function
   function loadCsv(csvPath) {
-      $.ajax({
-          type: 'GET',
-          url: csvPath,
-          dataType: 'text',
-          success: function(csvData) {
-              makeGeoJSON(csvData);
-          },
-          error: function(request, status, error) {
-              console.error('Error loading CSV:', error);
-          }
-      });
+    $.ajax({
+      type: 'GET',
+      url: csvPath,
+      dataType: 'text',
+      success: function(csvData) {
+        makeGeoJSON(csvData);
+      },
+      error: function(request, status, error) {
+        console.error('Error loading CSV:', error);
+      }
+    });
   }
+
+});
 
   // Function to assign different colors to CSV layers
   function getCsvColor(csvPath) {
