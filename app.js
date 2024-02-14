d6e734af1c00ec58f130393390b2f1b0291ebf48
Drazen Medina <drazenmedina@Drazens-MacBook-Pro.local>
2024-02-14 15:12:50 -0800

    Your commit message here

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function transformRequest(url) {
     url.slice(10, 26) === 'tiles.mapbox.com';
   return {
     url: isMapboxRequest ? url.replace('?', '?pluginName=finder&') : url,
-};
+  };
 }
 
 const map = new mapboxgl.Map({
@@ -53,8 +53,6 @@ function createPopup(currentFeature) {
     .addTo(map);
 }
 
-
-
 const geocoder = new MapboxGeocoder({
   accessToken: mapboxgl.accessToken,
   mapboxgl: mapboxgl,
@@ -65,7 +63,7 @@ const geocoder = new MapboxGeocoder({
 function sortByDistance(selectedPoint) {
   const options = { units: 'miles' };
   let data;
-  if (filteredGeojson.features.length > 0) {
+  if (filteredGeojson && filteredGeojson.features.length > 0) {
     data = filteredGeojson;
   } else {
     data = geojsonData;
@@ -144,265 +142,56 @@ map.on('load', () => {
   }
 
   // Function to make GeoJSON from CSV data
-function makeGeoJSON(csvData) {
-  // Split the CSV data into rows
-  const rows = csvData.split('\n');
+  function makeGeoJSON(csvData) {
+    // Split the CSV data into rows
+    const rows = csvData.split('\n');
 
-  // Initialize an array to store GeoJSON features
-  const features = [];
+    // Initialize an array to store GeoJSON features
+    const features = [];
 
-  // Iterate over each row of the CSV data
-  for (let i = 0; i < rows.length; i++) {
-    const row = rows[i];
+    // Iterate over each row of the CSV data
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i];
 
-    // Split the row into columns
-    const columns = row.split(',');
+      // Split the row into columns
+      const columns = row.split(',');
 
-    // Check if the row has at least two columns and latitude and longitude are not empty
-    if (columns.length >= 2 && columns[13].trim() !== '' && columns[14].trim() !== '') {
-      // Parse latitude and longitude values
-      const latitude = parseFloat(columns[13].trim());
-      const longitude = parseFloat(columns[14].trim());
+      // Check if the row has at least two columns and latitude and longitude are not empty
+      if (columns.length >= 2 && columns[13].trim() !== '' && columns[14].trim() !== '') {
+        // Parse latitude and longitude values
+        const latitude = parseFloat(columns[13].trim());
+        const longitude = parseFloat(columns[14].trim());
 
-      // Check if latitude and longitude values are valid numbers
-      if (!isNaN(latitude) && !isNaN(longitude)) {
-        // Create a GeoJSON feature
-        const feature = {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [longitude, latitude], // longitude, latitude
-          },
-          properties: {
-            id: i,
-          },
-        };
+        // Check if latitude and longitude values are valid numbers
+        if (!isNaN(latitude) && !isNaN(longitude)) {
+          // Create a GeoJSON feature
+          const feature = {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [longitude, latitude], // longitude, latitude
+            },
+            properties: {
+              id: i,
+            },
+          };
 
-        // Add the feature to the array
-        features.push(feature);
+          // Add the feature to the array
+          features.push(feature);
+        }
       }
     }
+
+    // Create a GeoJSON object from the features
+    geojsonData = {
+      type: 'FeatureCollection',
+      features: features,
+    };
+
+    // Add the GeoJSON layer to the map
+    addGeoJSONLayer();
   }
 
-  // Create a GeoJSON object from the features
-  const geojsonData = {
-    type: 'FeatureCollection',
-    features: features,
-  };
-
-  // Add the GeoJSON layer to the map
-  addGeoJSONLayer(geojsonData);
-}
-
-
-/*    function buildLocationList(locationData) {
-      const listings = document.getElementById('listings');
-      listings.innerHTML = '';
-      locationData.features.forEach((location, i) => {
-        const prop = location.properties;
-
-        const listing = listings.appendChild(document.createElement('div'));
-        listing.id = 'listing-' + prop.id;
-        listing.className = 'item';
-
-        const link = listing.appendChild(document.createElement('button'));
-        link.className = 'title';
-        link.id = 'link-' + prop.id;
-        link.innerHTML =
-          '<p style="line-height: 1.25">' + prop[columnHeaders[0]] + '</p>';
-
-        const details = listing.appendChild(document.createElement('div'));
-        details.className = 'content';
-
-        for (let i = 1; i < columnHeaders.length; i++) {
-          const div = document.createElement('div');
-          div.innerText += prop[columnHeaders[i]];
-          details.appendChild(div);
-        }
-
-        link.addEventListener('click', function () {
-          const clickedListing = location.geometry.coordinates;
-          flyToLocation(clickedListing);
-          createPopup(location);
-
-          const activeItem = document.getElementsByClassName('active');
-          if (activeItem[0]) {
-            activeItem[0].classList.remove('active');
-          }
-          this.parentNode.classList.add('active');
-
-          const divList = document.querySelectorAll('.content');
-          const divCount = divList.length;
-          for (let i = 0; i < divCount; i++) {
-            divList[i].style.maxHeight = null;
-          }
-
-          for (let i = 0; i < geojsonData.features.length; i++) {
-            this.parentNode.classList.remove('active');
-            this.classList.toggle('active');
-            const content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-              content.style.maxHeight = null;
-            } else {
-              content.style.maxHeight = content.scrollHeight + 'px';
-            }
-          }
-        });
-      });
-    }   */
-
-    // Function to add GeoJSON layer to the map
-    // Function to add GeoJSON layer to the map
-    function addGeoJSONLayer(locationData) {
-      // Check if the layer already exists and remove it before adding a new one
-      if (map.getLayer('locationData')) {
-        map.removeLayer('locationData');
-      }
-
-      map.addLayer({
-        id: 'locationData',
-        type: 'circle',
-        source: {
-          type: 'geojson',
-          data: locationData,
-        },
-        paint: {
-          'circle-radius': 5, // size of circles
-          'circle-color': '#3D2E5D', // color of circles
-          'circle-stroke-color': 'white',
-          'circle-stroke-width': 1,
-          'circle-opacity': 0.7,
-        },
-      });
-
-      // Set up event listeners for interacting with the map data
-      setMapEventListeners(locationData);
-    }
-
-    // Function to set up event listeners for interacting with the map data
-    function setMapEventListeners(locationData) {
-      // Event listener for clicking on map features
-      map.on('click', 'locationData', (e) => {
-        const features = map.queryRenderedFeatures(e.point, {
-          layers: ['locationData'],
-        });
-        if (!features.length) {
-          return;
-        }
-        const clickedPoint = features[0].geometry.coordinates;
-        flyToLocation(clickedPoint);
-        createPopup(features[0]);
-        const activeListing = document.getElementById(
-          'listing-' + features[0].properties.id
-        );
-        $(activeListing).addClass('active');
-        $(activeListing).siblings().removeClass('active');
-      });
-
-      // Event listeners for mouseenter and mouseleave on map features
-      map.on('mouseenter', 'locationData', () => {
-        map.getCanvas().style.cursor = 'pointer';
-      });
-
-      map.on('mouseleave', 'locationData', () => {
-        map.getCanvas().style.cursor = '';
-      });
-
-      // Build location list from GeoJSON data
-      buildLocationList(locationData);
-    }
-
-    // Function to build the location list
-    function buildLocationList(locationData) {
-      const listings = document.getElementById('listings');
-      listings.innerHTML = '';
-      locationData.features.forEach((location, i) => {
-        const prop = location.properties;
-
-        const listing = listings.appendChild(document.createElement('div'));
-        listing.id = 'listing-' + prop.id;
-        listing.className = 'item';
-
-        const link = listing.appendChild(document.createElement('button'));
-        link.className = 'title';
-        link.id = 'link-' + prop.id;
-        link.innerHTML =
-          '<p style="line-height: 1.25">' + prop[columnHeaders[0]] + '</p>';
-
-        const details = listing.appendChild(document.createElement('div'));
-        details.className = 'content';
-
-        for (let i = 1; i < columnHeaders.length; i++) {
-          const div = document.createElement('div');
-          div.innerText += prop[columnHeaders[i]];
-          details.appendChild(div);
-        }
-
-        link.addEventListener('click', function () {
-          const clickedListing = location.geometry.coordinates;
-          flyToLocation(clickedListing);
-          createPopup(location);
-
-          const activeItem = document.getElementsByClassName('active');
-          if (activeItem[0]) {
-            activeItem[0].classList.remove('active');
-          }
-          this.parentNode.classList.add('active');
-
-          const divList = document.querySelectorAll('.content');
-          const divCount = divList.length;
-          for (let i = 0; i < divCount; i++) {
-            divList[i].style.maxHeight = null;
-          }
-
-          for (let i = 0; i < locationData.features.length; i++) {
-            this.parentNode.classList.remove('active');
-            this.classList.toggle('active');
-            const content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-              content.style.maxHeight = null;
-            } else {
-              content.style.maxHeight = content.scrollHeight + 'px';
-            }
-          }
-        });
-      });
-    }
-
-    // Function to display error message
-    function displayErrorMessage(message) {
-      // You can implement how you want to display the error message to the user
-      alert(message);
-    }
-
-    // Call the function to load GeoJSON data from a file or an API endpoint
-    loadGeoJSONFromFile(config.CSV1);
-
-    // Function to load GeoJSON data from a file or an API endpoint
-    function loadGeoJSONFromFile(filePath) {
-      fetch(filePath)
-        .then(response => response.json())
-        .then(data => {
-          // Once the GeoJSON data is loaded, add the GeoJSON layer to the map
-          addGeoJSONLayer(data);
-        })
-        .catch(error => {
-          console.error('Error loading GeoJSON:', error);
-          displayErrorMessage('Error loading GeoJSON. Please try again.');
-        });
-    }
-
-  // Function to display error message
-  function displayErrorMessage(message) {
-    // You can implement how you want to display the error message to the user
-    alert(message);
-  }
-
-  const existingSource = map.getSource('locationData');
-      if (existingSource) {
-        // Remove the existing source
-        map.removeSource('locationData');
-      }
   // Function to add GeoJSON layer to the map
   function addGeoJSONLayer() {
     // Check if the layer already exists and remove it before adding a new one
@@ -463,6 +252,63 @@ function makeGeoJSON(csvData) {
     buildLocationList(geojsonData);
   }
 
+  // Function to build the location list
+  function buildLocationList(locationData) {
+    const listings = document.getElementById('listings');
+    listings.innerHTML = '';
+    locationData.features.forEach((location, i) => {
+      const prop = location.properties;
+
+      const listing = listings.appendChild(document.createElement('div'));
+      listing.id = 'listing-' + prop.id;
+      listing.className = 'item';
+
+      const link = listing.appendChild(document.createElement('button'));
+      link.className = 'title';
+      link.id = 'link-' + prop.id;
+      link.innerHTML =
+        '<p style="line-height: 1.25">' + prop[columnHeaders[0]] + '</p>';
+
+      const details = listing.appendChild(document.createElement('div'));
+      details.className = 'content';
+
+      for (let i = 1; i < columnHeaders.length; i++) {
+        const div = document.createElement('div');
+        div.innerText += prop[columnHeaders[i]];
+        details.appendChild(div);
+      }
+
+      link.addEventListener('click', function () {
+        const clickedListing = location.geometry.coordinates;
+        flyToLocation(clickedListing);
+        createPopup(location);
+
+        const activeItem = document.getElementsByClassName('active');
+        if (activeItem[0]) {
+          activeItem[0].classList.remove('active');
+        }
+        this.parentNode.classList.add('active');
+
+        const divList = document.querySelectorAll('.content');
+        const divCount = divList.length;
+        for (let i = 0; i < divCount; i++) {
+          divList[i].style.maxHeight = null;
+        }
+
+        for (let i = 0; i < locationData.features.length; i++) {
+          this.parentNode.classList.remove('active');
+          this.classList.toggle('active');
+          const content = this.nextElementSibling;
+          if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + 'px';
+          }
+        }
+      });
+    });
+  }
+
   // Add event listeners for sidebar toggle arrows
   document.getElementById('toggleLeft').addEventListener('click', function () {
     document.getElementById('sidebarB').classList.add('collapsed');
@@ -476,6 +322,11 @@ function makeGeoJSON(csvData) {
   loadCsv(config.CSV1);
 });
 
+// Function to display error message
+function displayErrorMessage(message) {
+  // You can implement how you want to display the error message to the user
+  alert(message);
+}
 
 // Function to toggle sidebar visibility
 $('#menu-toggle').on('click', function(e) {
